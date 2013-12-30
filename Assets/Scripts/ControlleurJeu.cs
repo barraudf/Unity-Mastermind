e3cc2e1d4e551d5f6da49a12107bd23d44462227
fabien <fabien@978c2c67-7d60-46f8-9efe-8416f885881f>
2013-12-30 23:04:10 +0000

    Ajout de la fonction de défilement dans l'historique des lignes pour pouvoir tout visualiser quand la taille de l'historique dépasse l'écran
    git-svn-id: svn+ssh://ironman/mnt/raid/data/svn/Unity/Mastermind/trunk@11 978c2c67-7d60-46f8-9efe-8416f885881f

--- Assets/Scripts/ControlleurJeu.cs
+++ Assets/Scripts/ControlleurJeu.cs
@@ -36,12 +36,28 @@ public class ControlleurJeu : Singleton<ControlleurJeu>
 	protected GameObject _EmplacementHistorique;
 	private ControlleurJeu() {}
 	private Rect _EmplacementBoutonValider;
+	private BoxCollider2D _ZoneDefilable;
+	private float _MinDefilement;
+	private float _MaxDefilement;
+
+	public float MinDefilement
+	{
+		get { return _MinDefilement; }
+	}
+	
+	public float MaxDefilement
+	{
+		get { return _MaxDefilement; }
+	}
 
 	void Start()
 	{
 		_EmplacementHistorique = (GameObject)GameObject.Find("EmplacementHistorique");
 		if(_EmplacementHistorique == null)
 			throw new UnityException("Impossible de trouver le GameObject EmplacementHistorique");
+		_ZoneDefilable = _EmplacementHistorique.GetComponent<BoxCollider2D>();
+		if(_ZoneDefilable == null)
+			throw new UnityException("Le GameObject EmplacementHistorique ne possède pas de composant BoxCollider2d");
 
 		if(PrefabPion == null)
 			throw new UnityException("Le Prefab Pion doit etre initialisé");
@@ -160,10 +176,18 @@ public class ControlleurJeu : Singleton<ControlleurJeu>
 		}
 		_EmplacementHistorique.transform.position += Vector3.up * (HauteurPion + DistanceSeparationLignes);
 		ligne.transform.parent = _EmplacementHistorique.transform;
-		ligne.transform.localPosition = Vector3.down * indexLigne * (HauteurPion + DistanceSeparationLignes);
+		ligne.transform.localPosition = Vector3.down * (indexLigne + 1) * (HauteurPion + DistanceSeparationLignes);
 
 		AjouterVerif(ligne, bienPlace, malPlace);
 		AjouterNumeroLigne(ligne, indexLigne+1);
+
+		MettreAJourZoneDefilable();
+
+		_MaxDefilement = _EmplacementHistorique.transform.position.y;
+		if(_MaxDefilement > Camera.main.orthographicSize * 2)
+			_MinDefilement = Camera.main.orthographicSize * 2;
+		else
+			_MinDefilement = _MaxDefilement;
 	}
 
 	public void AjouterNumeroLigne(GameObject ligne, int numLigne)
@@ -180,7 +204,7 @@ public class ControlleurJeu : Singleton<ControlleurJeu>
 	public void AjouterVerif(GameObject ligne, int bienPlace, int malPlace)
 	{
 		int nombrePionsVerifParLigne = Mathf.CeilToInt(TailleCodeSecret / 2);
-		float xDepartVerif = TailleCodeSecret * LargeurPion + TailleCodeSecret * DistanceSeparationPions;
+		float xDepartVerif = TailleCodeSecret * (LargeurPion + DistanceSeparationPions);
 		float largeurVerif = SpriteEmplacementVerif.rect.width;
 		float hauteurVerif = SpriteEmplacementVerif.rect.height;
 
@@ -251,5 +275,15 @@ public class ControlleurJeu : Singleton<ControlleurJeu>
 		Vector3 positionElement = Camera.main.WorldToScreenPoint(new Vector3(x, y, 0f));
 		positionElement.y = Screen.height - positionElement.y;
 		return positionElement;
+	}
+
+	public void MettreAJourZoneDefilable()
+	{
+		float hauteur, largeur;
+		hauteur = _EmplacementHistorique.transform.childCount * (HauteurPion + DistanceSeparationLignes);
+		float largeurVerif = SpriteVerifBienPlacee.rect.width;
+		largeur = TailleCodeSecret * (LargeurPion + DistanceSeparationPions) + Mathf.CeilToInt(TailleCodeSecret / 2) * largeurVerif + 50f;
+		_ZoneDefilable.size = new Vector2(largeur, hauteur);
+		_ZoneDefilable.center = new Vector2(_ZoneDefilable.size.x / 2 - 30f, -(HauteurPion+DistanceSeparationLignes)/2 *_EmplacementHistorique.transform.childCount);
 	}
 }
---
+++ Assets/Scripts/Defiler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defiler : MonoBehaviour
+{
+	private Vector3 _PositionSourisCyclePrecedent = Vector3.zero;
+	private bool _DefilementEnCours = false;
+
+	void OnMouseDown()
+	{
+		_PositionSourisCyclePrecedent = Input.mousePosition;
+		_DefilementEnCours = true;
+	}
+	
+	void OnMouseDrag()
+	{
+		if(_DefilementEnCours == true)
+		{
+			transform.position += Vector3.up * (Input.mousePosition.y - _PositionSourisCyclePrecedent.y);
+			_PositionSourisCyclePrecedent = Input.mousePosition;
+
+			if(transform.position.y < ControlleurJeu.Instance.MinDefilement)
+				transform.position = new Vector2(transform.position.x, ControlleurJeu.Instance.MinDefilement);
+			if(transform.position.y > ControlleurJeu.Instance.MaxDefilement)
+				transform.position = new Vector2(transform.position.x, ControlleurJeu.Instance.MaxDefilement);
+		}
+	}
+
+	void OnMouseUp()
+	{
+		_DefilementEnCours = false;
+	}
+}
